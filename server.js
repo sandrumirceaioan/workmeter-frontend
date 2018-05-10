c440784ae1533e4323061941b88327dcf029d3f6
Sandru Mircea <sandrumirceaioan@gmail.com>
2018-05-10 14:20:58 +0300

    jhkjhkjhkjhk

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
-// Require express
-const express = require('express');
-const path = require('path');
+// // Require express
+// const express = require('express');
+// const path = require('path');
 
-const app = express();
+// const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+// // Serve only the static files form the dist directory
+// app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req,res) {
+// app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/index.html'));
-});
+// res.sendFile(path.join(__dirname+'/dist/index.html'));
+// });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
+
+var express = require('express');
+var httpProxy = require('http-proxy');
+var apiForwardingUrl = 'https://workmeter-backend.herokuapp.com';
+var server = express();
+server.set('port', 3000);
+server.use(express.static(__dirname + '/app'));
+var apiProxy = httpProxy.createProxyServer();
+
+server.listen(process.env.PORT || 8080);
