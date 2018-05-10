e4dcce9e0b5bedaa921a8723c1cd8c851a647dc1
Sandru Mircea <sandrumirceaioan@gmail.com>
2018-05-10 14:54:36 +0300

    hgjghjghjghj

--- server.js
+++ server.js
@@ -16,14 +16,21 @@
 // app.listen(process.env.PORT || 8080);
 
 var express = require('express');
+var path = require('path');
 var httpProxy = require('http-proxy');
-var apiForwardingUrl = 'https://workmeter-backend.herokuapp.com';
-var server = express();
-server.set('port', 3000);
-server.use(express.static(__dirname + '/dist'));
-server.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+'/dist/index.html'));
-});
-var apiProxy = httpProxy.createProxyServer();
+var apiForwardingUrl = "https://workmeter-backend.herokuapp.com";
 
+var server = express();
+server.get('/*', function(req,res) {
+    
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
+    });
+
+var apiProxy = httpProxy.createProxyServer();
+console.log('Forwarding API requests to ' + apiForwardingUrl);
+server.all("/api/*", function(req, res) {
+    apiProxy.web(req, res, {target: apiForwardingUrl});
+});
+
+// Start the app by listening on the default Heroku port
 server.listen(process.env.PORT || 8080);
