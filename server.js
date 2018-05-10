6afb1c7c7b0b5321e06b36c45f9937a48b8b243e
Sandru Mircea <sandrumirceaioan@gmail.com>
2018-05-10 15:35:59 +0300

    fdgdfgdfgdfgdfg

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
-// Require express
-const express = require('express');
-const path = require('path');
+// // Require express
+// const express = require('express');
+// const path = require('path');
 
-const app = express();
+// const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+// // Serve only the static files form the dist directory
+// app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req,res) {
-   res.sendFile(path.join(__dirname+'/dist/index.html'));
-});
+// app.get('/*', function(req,res) {
+    
+// res.sendFile(path.join(__dirname+'/dist/index.html'));
+// });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
+
+var express = require('express'),
+    proxy = require('http-proxy-middleware'),
+    app = express(),
+    path = require('path');
+
+setTimeout(() => {
+    app.use(proxy('/api', { target: 'https://workmeter-backend.herokuapp.com', changeOrigin: false }));
+    app.use(express.static(path.join(__dirname, 'dist')));
+    app.get('*', function (req, res) {
+        res.sendFile(__dirname + '/dist/index.html');
+    });
+    var server = require('http').createServer(app);
+    server.listen(process.env.PORT || 8080);
+}, 3000);
