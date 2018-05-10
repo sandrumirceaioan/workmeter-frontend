6f351d22b4a1529c2e48ace1725424a17388d9a9
Sandru Mircea <sandrumirceaioan@gmail.com>
2018-05-10 14:28:13 +0300

    dsdsdsd

--- server.js
+++ server.js
@@ -21,6 +21,9 @@ var apiForwardingUrl = 'https://workmeter-backend.herokuapp.com';
 var server = express();
 server.set('port', 3000);
 server.use(express.static(__dirname + '/dist'));
+server.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
 var apiProxy = httpProxy.createProxyServer();
 
 server.listen(process.env.PORT || 8080);
